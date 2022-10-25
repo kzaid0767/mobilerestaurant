a370dc262aa7c060b2ebd1902f9547a1db828517
Kassim Zaid <kzaid0767@gmail.com>
2022-10-25 13:23:01 -0400

    app finalized

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 import { menuArray } from "./data.js";
+const paymentForm = document.getElementById('paymentform')
+const thanksMessage = document.getElementById('thanks')
+const ratingDiv = document.querySelector('#ratingDiv')
+const offerBtn = document.getElementById("offerBtn")
+
 let pizzaCount=0
 let burgerCount=0
 let friesCount=0
 let mocktailCount=0
 let total=0
+let discountTotal = 0
+let friesHtml=``
+let mocktailHtml=``
+let pizzaHtml =``
+let burgerHtml=``
+
 
 
 document.body.addEventListener('click', function(e){
     if(e.target.dataset.key){
         handleIncrement(e.target.dataset.key)
-    } else (e.target.dataset.rkey){
+    } else if (e.target.dataset.rkey){
         handleRemove(e.target.dataset.rkey)
+    } else if (e.target.dataset.pay){
+        handlePay()
     }
     
 })
 
 
+paymentForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    const paymentInfo = new FormData(paymentForm)
+    document.getElementById('yourOrderDiv').style.display='none'
+    thanksMessage.textContent=`Thanks, ${paymentInfo.get('name')}! Your order is on its way`
+    thanksMessage.style.display='block'
+    ratingDiv.style.display = 'block'
+    paymentForm.style.display = 'none'
+    paymentForm.reset() 
+    pizzaCount=0
+    burgerCount=0
+    friesCount=0
+    mocktailCount=0
+    total=0
+    friesHtml=``
+    mocktailHtml=``
+    pizzaHtml =``
+    burgerHtml=``
+})
 
-let friesHtml=``
-let mocktailHtml=``
-let pizzaHtml =``
-let burgerHtml=``
+
 function handleIncrement(id){
-    let orderHtml = ``
-    
+    thanksMessage.style.display='none' //closing if it previous open
+
     id=parseInt(id)
     const itemObj = menuArray.filter(item=>
         item.id === id)[0]    
@@ -32,7 +61,7 @@ function handleIncrement(id){
         pizzaCount++
         pizzaHtml = `
         <div class="listedItems">
-            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients" data-rkey="${id}">remove</span></span> 
+            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients remove" data-rkey="${id}">remove</span></span> 
             <span>$${itemObj.price*pizzaCount}</span>
         </div>
         `
@@ -41,7 +70,7 @@ function handleIncrement(id){
         burgerCount++
         burgerHtml = `
         <div class="listedItems">
-            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients">remove</span></span> 
+            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients remove" data-rkey="${id}">remove</span></span> 
             <span>$${itemObj.price*burgerCount}</span>
         </div>
         `
@@ -50,7 +79,7 @@ function handleIncrement(id){
         friesCount++
         friesHtml = `
         <div class="listedItems">
-            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients">remove</span></span> 
+            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients remove" data-rkey="${id}">remove</span></span> 
             <span>$${itemObj.price*friesCount}</span>
         </div>
         `
@@ -59,22 +88,86 @@ function handleIncrement(id){
         mocktailCount++
         mocktailHtml = `
         <div class="listedItems">
-            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients">remove</span></span> 
+            <span class="eachItem-details name">${itemObj.name} <span class="eachItem-details ingredients remove" data-rkey="${id}">remove</span></span> 
             <span>$${itemObj.price*mocktailCount}</span>
         </div>
         `
         total +=itemObj.price
     }
+    
+    renderYourOrder()
+}
+
+function handleRemove(id){
+    id=parseInt(id)
+    const itemObj = menuArray.filter(item=>
+        item.id === id)[0] 
+    if(id===0){
+        total -= itemObj.price*pizzaCount
+        pizzaCount=0
+        pizzaHtml=``
+    
+    } else if (id===1){
+        total -= itemObj.price*burgerCount
+        burgerCount=0
+        burgerHtml=``
+    
+    } else if (id===2){
+        total -= itemObj.price*friesCount
+        friesCount=0
+        friesHtml=``
+    
+    } else if (id===3){
+        total -= itemObj.price*mocktailCount
+        mocktailCount=0
+        mocktailHtml=``
+    
+    }
+    renderYourOrder()
+}
+
+function handlePay(){
+    paymentForm.style.display='block'
+}
+
+ratingDiv.addEventListener('click', function (e) {
+    const targetId = e.target.id
+    const star1 = document.getElementById('star1')
+    const star2 = document.getElementById('star2')
+    const star3 = document.getElementById('star3')
+    const star4 = document.getElementById('star4')
+    const star5 = document.getElementById('star5')
+    if(targetId==='star1'){
+        star1.classList.toggle('ratingColor')
+    } else if (targetId==='star2'){
+        star2.classList.toggle('ratingColor')
+    } else if (targetId==='star3'){
+        star3.classList.toggle('ratingColor')
+    } else if (targetId==='star4'){
+        star4.classList.toggle('ratingColor')
+    } else if (targetId==='star5'){
+        star5.classList.toggle('ratingColor')
+    } else if(targetId==='ratingBtn'){
+        ratingDiv.style.display='none'
+        document.getElementById('offerDiv').style.display='block'
+    }
+    
+})
+
+offerBtn.addEventListener('click',()=>{
+    document.getElementById('offerDiv').style.display='none'
+})
+
+function renderYourOrder(){
+    total>0?document.getElementById('yourOrderDiv').style.display='block':document.getElementById('yourOrderDiv').style.display='none'
+    total>=30?discountTotal = (total*0.9).toFixed(2): discountTotal= total
+    let orderHtml=``
     orderHtml = `<div class="listedItems">
+    <span class="eachItem-details name">Discounted price:</span><span>$${discountTotal}</span>
     <span class="eachItem-details name">Total price:</span><span>$${total}</span>
     </div>`
     document.getElementById('toCheckOut').innerHTML = pizzaHtml+burgerHtml+friesHtml+mocktailHtml
     document.getElementById('total').innerHTML = orderHtml
-    
-}
-
-function handleRemove(){
-
 }
 
 function getHTML(){
@@ -83,7 +176,7 @@ function getHTML(){
     menuArray.forEach(item=>
         feedHtml += `
         <div class="eachItem">
-            <img src="${item.emoji}" class="emoji" alt="">
+            <img src="${item.emoji}" class="emoji" alt="${item.name}">
             <div class="item-text">
                 <h1 class="eachItem-details name">${item.name}</h1>
                 <p class="eachItem-details ingredients">${item.ingredients}</p>
@@ -100,5 +193,10 @@ function getHTML(){
 function render(){
     document.querySelector('#items').innerHTML = getHTML()
 }
+
+setInterval( () => { 
+    const blinker = document.getElementById('blinker')
+    blinker.style.visibility = (blinker.style.visibility == 'hidden' ? ''
+  : 'hidden')}, 750)
 
 render()
