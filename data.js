50538411edcc2c7835e82d489f77ee6253e3eeb7
Kassim Zaid <kzaid0767@gmail.com>
2022-10-26 15:31:35 -0400

    weird left alignment solved

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["veggies,mushrom,mozarella"],
+        ingredients: ["pepperoni","mushrom","mozarella"],
         id: 0,
         price: 14,
         emoji: `images/pizza.jpg`
